648f390037fcdc8929e25ac116023bd0df3101a5
Ivan Zapryanov <50863199+Zapryanov@users.noreply.github.com>
2020-11-21 15:00:51 +0200

    feat: added few service files

--- workshop-components/ng-workshop/src/app/app.module.ts
+++ workshop-components/ng-workshop/src/app/app.module.ts
@@ -7,6 +7,8 @@ import { FooterComponent } from './footer/footer.component';
 import { ThemeListComponent } from './theme-list/theme-list.component';
 import { AsideComponent } from './aside/aside.component';
 import { ThemeListItemComponent } from './theme-list-item/theme-list-item.component';
+import { UserService } from './user.service';
+import { storageServiceProvider } from './storage.service';
 
 @NgModule({
   declarations: [
@@ -20,7 +22,10 @@ import { ThemeListItemComponent } from './theme-list-item/theme-list-item.compon
   imports: [
     BrowserModule
   ],
-  providers: [],
+  providers: [
+    UserService,
+    storageServiceProvider
+  ],
   bootstrap: [
     AppComponent,
     HeaderComponent,
