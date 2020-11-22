78ae7d21dc233b64dedd7da1d71da2b6883c4236
Ivan Zapryanov <50863199+Zapryanov@users.noreply.github.com>
2020-11-22 10:46:55 +0200

    feat: add Core and Share modules and Home and About components

--- app-with-router/src/app/app-routing.module.ts
+++ app-with-router/src/app/app-routing.module.ts
@@ -1,10 +1,25 @@
-import { NgModule } from '@angular/core';
+// import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
+import { AboutComponent } from './about/about.component';
+import { HomeComponent } from './home/home.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {
+    path: "",
+    pathMatch: "full",
+    component: HomeComponent
+  },
+  {
+    path: "about",
+    component: AboutComponent
+  }
+];
 
-@NgModule({
-  imports: [RouterModule.forRoot(routes)],
-  exports: [RouterModule]
-})
-export class AppRoutingModule { }
+export const AppRoutingModule = RouterModule.forRoot(routes);
+
+
+// @NgModule({
+//   imports: [RouterModule.forRoot(routes)],
+//   exports: [RouterModule]
+// })
+// export class AppRoutingModule { }
