13bdde0504dc2c671865e85ac96fe8ceb9375c2e
Ivan Zapryanov <50863199+Zapryanov@users.noreply.github.com>
2020-11-22 11:17:27 +0200

    feat: add loader and navigation components

--- app-with-router/src/app/app.module.ts
+++ app-with-router/src/app/app.module.ts
@@ -5,6 +5,8 @@ import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { HomeComponent } from './home/home.component';
 import { AboutComponent } from './about/about.component';
+import { CoreModule } from './core/core.module';
+import { SharedModule } from './shared/shared.module';
 
 @NgModule({
   declarations: [
@@ -14,7 +16,9 @@ import { AboutComponent } from './about/about.component';
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    CoreModule,
+    SharedModule
   ],
   providers: [],
   bootstrap: [AppComponent]
