1ba3f127071c1fad0ebae1c38113a4b996f018a0
Ivan Zapryanov <50863199+Zapryanov@users.noreply.github.com>
2020-11-23 21:38:59 +0200

    feat: successfully take data with Observable

--- app-with-router/src/app/user/user.module.ts
+++ app-with-router/src/app/user/user.module.ts
@@ -3,6 +3,7 @@ import { CommonModule } from '@angular/common';
 import { ListComponent } from './list/list.component';
 import { DetailComponent } from './detail/detail.component';
 import { UserRoutingModule } from './user-routing.module';
+import { SharedModule } from '../shared/shared.module';
 
 
 
@@ -13,7 +14,8 @@ import { UserRoutingModule } from './user-routing.module';
   ],
   imports: [
     CommonModule,
-    UserRoutingModule
+    UserRoutingModule,
+    SharedModule
   ]
 })
 export class UserModule { }
