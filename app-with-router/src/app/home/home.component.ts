63b8ec50e81a9b130b1abeea202ad1f968dc8192
Ivan Zapryanov <50863199+Zapryanov@users.noreply.github.com>
2020-11-23 09:23:06 +0200

    feat: example of unsubscribe for observables

--- app-with-router/src/app/home/home.component.ts
+++ app-with-router/src/app/home/home.component.ts
@@ -1,15 +1,22 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnDestroy } from '@angular/core';
+import { interval, Subscription } from 'rxjs';
 
 @Component({
   selector: 'app-home',
   templateUrl: './home.component.html',
   styleUrls: ['./home.component.css']
 })
-export class HomeComponent implements OnInit {
+export class HomeComponent implements OnDestroy {
 
-  constructor() { }
+  counter = 0;
+  intervalSubscribtion: Subscription;
 
-  ngOnInit(): void {
+  constructor() {
+    this.intervalSubscribtion = interval(1000).subscribe(() => { this.counter++; });
+  }
+
+  ngOnDestroy(): void {
+    this.intervalSubscribtion.unsubscribe();
   }
 
 }
