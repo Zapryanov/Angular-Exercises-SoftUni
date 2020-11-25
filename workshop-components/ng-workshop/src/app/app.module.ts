72b906e693140dbfee98fec266c0d5bff90a3ad5
Ivan Zapryanov <50863199+Zapryanov@users.noreply.github.com>
2020-11-25 14:28:18 +0200

    feat: move Header and Footer components in the Core module

--- workshop-components/ng-workshop/src/app/app.module.ts
+++ workshop-components/ng-workshop/src/app/app.module.ts
@@ -3,8 +3,8 @@ import { NgModule } from '@angular/core';
 import { HttpClientModule } from '@angular/common/http';
 
 import { AppComponent } from './app.component';
-import { HeaderComponent } from './header/header.component';
-import { FooterComponent } from './footer/footer.component';
+import { HeaderComponent } from './core/header/header.component';
+import { FooterComponent } from './core/footer/footer.component';
 import { ThemeListComponent } from './theme-list/theme-list.component';
 import { AsideComponent } from './aside/aside.component';
 import { ThemeListItemComponent } from './theme-list-item/theme-list-item.component';
@@ -15,18 +15,18 @@ import { PostService } from './post.service';
 import { HomeComponent } from './home/home.component';
 
 import { AppRoutingModule } from './app-routing.module';
+import { CoreModule } from './core/core.module';
 
 @NgModule({
   declarations: [
     AppComponent,
-    HeaderComponent,
-    FooterComponent,
     ThemeListComponent,
     AsideComponent,
     ThemeListItemComponent,
     HomeComponent
   ],
   imports: [
+    CoreModule,
     BrowserModule,
     HttpClientModule,
     AppRoutingModule
