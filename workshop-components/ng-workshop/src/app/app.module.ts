4b5a66950709e618327f662b034ec878c33d6f91
Ivan Zapryanov <50863199+Zapryanov@users.noreply.github.com>
2020-11-26 06:32:47 +0200

    feat: created the components: footer, theme-list and aside

--- workshop-components/ng-workshop/src/app/app.module.ts
+++ workshop-components/ng-workshop/src/app/app.module.ts
@@ -3,11 +3,17 @@ import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
 import { HeaderComponent } from './header/header.component';
+import { FooterComponent } from './footer/footer.component';
+import { ThemeListComponent } from './theme-list/theme-list.component';
+import { AsideComponent } from './aside/aside.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     HeaderComponent,
+    FooterComponent,
+    ThemeListComponent,
+    AsideComponent,
   ],
   imports: [
     BrowserModule
