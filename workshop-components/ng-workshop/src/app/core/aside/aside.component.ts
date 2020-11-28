0a2d67e806faae153d84afa722228d23fc3b4c17
Ivan Zapryanov <50863199+Zapryanov@users.noreply.github.com>
2020-11-28 15:39:51 +0200

    feat: moved aside component into core module and theme-list into theme module

--- workshop-components/ng-workshop/src/app/core/aside/aside.component.ts
+++ workshop-components/ng-workshop/src/app/core/aside/aside.component.ts
@@ -1,4 +1,4 @@
-import { Component } from '@angular/core';
+import { Component, Input } from '@angular/core';
 
 @Component({
   selector: 'app-aside',
@@ -7,6 +7,6 @@ import { Component } from '@angular/core';
 })
 export class AsideComponent {
 
-
+  @Input() title: string;
 
 }
