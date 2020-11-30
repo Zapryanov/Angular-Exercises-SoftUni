f12ef344c7cd9a8856a38830413c2e151cf8c184
Ivan Zapryanov <50863199+Zapryanov@users.noreply.github.com>
2020-11-30 17:34:34 +0200

    feat: login component was created

--- workshop-components/ng-workshop/src/app/user/user.module.ts
+++ workshop-components/ng-workshop/src/app/user/user.module.ts
@@ -2,11 +2,12 @@ import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { ProfileComponent } from './profile/profile.component';
 import { UserService } from './user.service';
+import { LoginComponent } from './login/login.component';
 
 
 
 @NgModule({
-  declarations: [ProfileComponent],
+  declarations: [ProfileComponent, LoginComponent],
   imports: [
     CommonModule
   ],
