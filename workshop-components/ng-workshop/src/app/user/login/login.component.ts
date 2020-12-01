b7e6b875400851937a7a0fc8f745d2fcc4a535fd
Ivan Zapryanov <50863199+Zapryanov@users.noreply.github.com>
2020-12-01 10:24:57 +0200

    feat: logic has been added into login

--- workshop-components/ng-workshop/src/app/user/login/login.component.ts
+++ workshop-components/ng-workshop/src/app/user/login/login.component.ts
@@ -7,9 +7,41 @@ import { Component, OnInit } from '@angular/core';
 })
 export class LoginComponent implements OnInit {
 
+  form = {
+    email: {
+      touched: false,
+      value: ""
+    },
+    password: {
+      touched: false,
+      value: ""
+    }
+  };
+
+  get showEmailError(): boolean {
+    return this.form.email.touched && this.form.email.value.length === 0;
+  }
+
+  get showPasswordError(): boolean {
+    return this.form.password.touched && this.form.password.value.length === 0;
+  }
+
+  get hasFormErrors(): boolean {
+    return this.form.email.value.length === 0 || this.form.password.value.length === 0;
+  }
+
   constructor() { }
 
   ngOnInit(): void {
+  }
+
+  updateInputValue(name: "email" | "password", value: string): void {
+    this.form[name].touched = true;
+    this.form[name].value = value;
+  }
+
+  submitFormHandler(): void {
+
   }
 
 }
