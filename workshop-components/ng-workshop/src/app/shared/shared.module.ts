c854cc29a0c3f9015f5c5a4d4272078a70405213
Ivan Zapryanov <50863199+Zapryanov@users.noreply.github.com>
2020-12-01 06:42:28 +0200

    feat: HTML and CSS has been added to login component and also the first directive

--- workshop-components/ng-workshop/src/app/shared/shared.module.ts
+++ workshop-components/ng-workshop/src/app/shared/shared.module.ts
@@ -1,10 +1,11 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
+import { IsEmptyDirective } from './is-empty.directive';
 
 
 
 @NgModule({
-  declarations: [],
+  declarations: [IsEmptyDirective],
   imports: [
     CommonModule
   ]
