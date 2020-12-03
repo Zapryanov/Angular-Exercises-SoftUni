6382ec7d59391f5a182e6becda51f1c57d5541b2
Ivan Zapryanov <50863199+Zapryanov@users.noreply.github.com>
2020-12-03 05:03:42 +0200

    feat: home component was created

--- forms-app/src/app/app.module.ts
+++ forms-app/src/app/app.module.ts
@@ -4,15 +4,19 @@ import { NgModule } from '@angular/core';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { SharedModule } from './shared/shared.module';
+import { UserModule } from './user/user.module';
+import { HomeComponent } from './home/home.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HomeComponent
   ],
   imports: [
     BrowserModule,
     AppRoutingModule,
-    SharedModule
+    SharedModule,
+    UserModule
   ],
   providers: [],
   bootstrap: [AppComponent]
