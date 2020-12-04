81faa5b360676110534f4966a1b13cf8f90ab545
Ivan Zapryanov <50863199+Zapryanov@users.noreply.github.com>
2020-12-04 08:36:05 +0200

    feat: register component was created

--- forms-app/src/app/app.module.ts
+++ forms-app/src/app/app.module.ts
@@ -7,11 +7,13 @@ import { SharedModule } from './shared/shared.module';
 import { UserModule } from './user/user.module';
 import { HomeComponent } from './home/home.component';
 import { CoreModule } from './core/core.module';
+import { RegisterComponent } from './register/register.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    HomeComponent
+    HomeComponent,
+    RegisterComponent
   ],
   imports: [
     BrowserModule,
