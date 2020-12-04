ecdbb097705ffbee2d66f0283a06d71e044d43c2
Ivan Zapryanov <50863199+Zapryanov@users.noreply.github.com>
2020-12-04 15:12:37 +0200

    feat: global css styles was added

--- forms-app/src/app/user/login/login.component.ts
+++ forms-app/src/app/user/login/login.component.ts
@@ -4,7 +4,7 @@ import { NgForm } from '@angular/forms';
 @Component({
   selector: 'app-login',
   templateUrl: './login.component.html',
-  styleUrls: ['./login.component.css']
+  styleUrls: ['./login.component.css', '../../../form-styles.css']
 })
 export class LoginComponent implements OnInit, AfterViewInit {
 
