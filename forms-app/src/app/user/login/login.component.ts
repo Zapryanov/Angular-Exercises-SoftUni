34878d374781e447a8192e63f4695d919f31aa2a
Ivan Zapryanov <50863199+Zapryanov@users.noreply.github.com>
2020-12-04 08:27:29 +0200

    feat: login form was added

--- forms-app/src/app/user/login/login.component.ts
+++ forms-app/src/app/user/login/login.component.ts
@@ -1,15 +1,27 @@
-import { Component, OnInit } from '@angular/core';
+import { AfterViewInit, Component, OnInit, ViewChild } from '@angular/core';
+import { NgForm } from '@angular/forms';
 
 @Component({
   selector: 'app-login',
   templateUrl: './login.component.html',
   styleUrls: ['./login.component.css']
 })
-export class LoginComponent implements OnInit {
+export class LoginComponent implements OnInit, AfterViewInit {
+
+  @ViewChild("f") form: NgForm;
 
   constructor() { }
 
   ngOnInit(): void {
+  }
+
+  ngAfterViewInit(): void {
+    this.form.valueChanges.subscribe(console.log);
+  }
+
+  loginHandler(formData) {
+    console.log(this.form);
+    console.log(formData);
   }
 
 }
